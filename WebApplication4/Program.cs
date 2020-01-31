4aba48790d57acf7c7187088a2129d6927fb9219
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-01-31 10:11:19 +0100

    Final experimentation commit before slimming down solution

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Configuration;
@@ -16,22 +17,45 @@ namespace WebApplication4
     {
         public static void Main(string[] args)
         {
-            //asdf();
+            //asdf2();
             //Console.WriteLine("asdf");
+
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static void asdf()
+        //public static async void asdf()
+        //{
+        //    var ac = "https://localhost:8081";
+        //    var ke = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw///Jw==";
+        //
+        //    IDocumentClient client = new DocumentClient(new Uri(ac), ke);
+        //    Database db = client.CreateDatabaseIfNotExistsAsync(new Database { Id = "MyDatabase" }).Result;
+        //
+        //    var collection = await client.CreateDocumentCollectionIfNotExistsAsync(db.SelfLink, new //DocumentCollection { Id = "asdf" });
+        //
+        //    Console.WriteLine(db);
+        //}
+
+        public static async void asdf2()
         {
             var ac = "https://localhost:8081";
             var ke = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+            var dbName = "MyDatabase";
+            var containerName = "MyContainer";
 
-            IDocumentClient client = new DocumentClient(new Uri(ac), ke);
-            Database db = client.CreateDatabaseIfNotExistsAsync(new Database { Id = "MyDatabase" }).Result;
+            var client = new CosmosClient(ac, ke);
 
-            client.CreateDocumentCollectionIfNotExistsAsync(db.SelfLink, new DocumentCollection { Id = "asdf" });
-            
-            Console.WriteLine(db);
+            _ = await client.CreateDatabaseIfNotExistsAsync(dbName);
+            var db = client.GetDatabase(dbName);
+            var c = await db.CreateContainerAsync(new ContainerProperties { Id = containerName });
+            var container = c.Container;
+
+            _ = await container.CreateItemAsync(new Skill { Id = "1", Name = "Test" });
+
+            var query = container.GetItemLinqQueryable<Skill>().Select(a => a.Name);
+
+            foreach(var a in query)
+                Console.WriteLine(a);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
